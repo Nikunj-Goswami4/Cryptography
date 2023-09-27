1ff5d8f703e8119e932a9af94b0b51376ee71f3e
Nikunj Goswami <goswaminikunj2003@gmail.com>
2023-09-27 23:31:37 +0530

    Made interface class , and 3 classes for Algos and one class to choose security level.

--- src/Cryptography.java
+++ src/Cryptography.java
@@ -1,12 +1,66 @@
+import java.util.Scanner;
+
 public class Cryptography {
     public static void main(String[] args){
 
-        //Beginner (Novice) ....Security (key : only adding character)
-        // Intermediate ....Security (key : adding character + randomizing that characters)
+        // Beginner (Novice) ....Security (key : only adding character)
+        // Intermediate ....Security (key : adding character in array + randomizing that characters in that array)
         // Advanced ....Security (AES)
 
+        //to make access modifiers PRIVATE from public as it's a program for security
 
+        System.out.println("---------------------------------");
+        System.out.println("\t\t CRPYPTOGRAPHY");
+        System.out.println("---------------------------------\n\n");
+
+        ChooseSecurityLevel CSL = new ChooseSecurityLevel();
 
     }
 }
 
+class ChooseSecurityLevel{
+    Scanner sc = new Scanner(System.in);
+
+    //Constructor
+    public ChooseSecurityLevel(){
+        System.out.print("1. Novice\n2. Intermediate\n3. Advanced");
+        System.out.print("\nChoose Security Level : ");
+        int level = sc.nextInt();
+
+        if (level==1){
+            CustomAlgo1 customAlgo1 = new CustomAlgo1();
+        }
+        else if (level==2) {
+            CustomAlgo2 customAlgo2 = new CustomAlgo2();
+        }
+        else if (level==3) {
+            AES aes = new AES();
+        }
+        else {
+            System.out.println("Please enter valid Security Level!!!");
+        }
+
+    }
+}
+
+//Interface Class
+interface ExecutionLayout{
+
+    public void askUser();
+    public void newKey();
+    public void getKey();
+    public void encrypt();
+    public void decrypt();
+}
+
+class CustomAlgo1{
+
+}
+
+class CustomAlgo2{
+
+}
+
+class AES{
+
+}
