a0e19aab52d03eabe8b2b12e034a0ea27f4fde12
Nikunj Goswami <goswaminikunj2003@gmail.com>
2023-09-27 23:00:26 +0530

    Initial commit

---
+++ src/Cryptography.java
@@ -0,0 +1,12 @@
+public class Cryptography {
+    public static void main(String[] args){
+
+        //Beginner (Novice) ....Security (key : only adding character)
+        // Intermediate ....Security (key : adding character + randomizing that characters)
+        // Advanced ....Security (AES)
+
+
+
+    }
+}
+
