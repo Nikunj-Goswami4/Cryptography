6070fe791e60237b532516b38c0e84a6afa26982
Nikunj Goswami <goswaminikunj2003@gmail.com>
2023-10-01 17:26:22 +0530

    Made Package and transfer all classes into it.
    CustomAlgo1 is completed.

---
+++ src/CryptographyAlgorithms/Cryptography.java
@@ -0,0 +1,50 @@
+package CryptographyAlgorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Cryptography {
+    public static void main(String[] args){
+
+        // Beginner (Novice) ....Security (key : only adding character)
+        // Intermediate ....Security (key : adding character in array + randomizing that characters in that array)
+        // Advanced ....Security (CryptographyAlgorithms.AES)
+
+        //to make access modifiers PRIVATE from public as it's a program for security
+
+        System.out.println("---------------------------------");
+        System.out.println("\t\t CRPYPTOGRAPHY");
+        System.out.println("---------------------------------\n");
+
+        ChooseSecurityLevel CSL = new ChooseSecurityLevel();
+
+//        ArrayList<String> text = new ArrayList<>();
+
+//        String inputString = "yourInputString";
+//        List<Character> charList = inputString.chars() // Get IntStream of characters
+//                .mapToObj(c -> (char) c) // Convert each int to char
+//                .toList(); // Collect into a List<Character>
+
+//        List<Character> chars = new ArrayList<>();
+
+        // For each character in the String
+        // add it to the List
+//        for (char ch : inputString.toCharArray()) {
+//            chars.add(ch);
+//        }
+//
+//        System.out.println(chars.get(0));
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
